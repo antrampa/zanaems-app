636dbb8a73c4522682f634d3c3ac668b84fb040c
Antonis Zalonis <antonis.zalonis@gmail.com>
2025-01-18 17:17:43 +0200

    users index

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -15,8 +15,7 @@
                     {{Session::get('message')}}
                 </div>
             @endif
-
-            <table class="table table-bordered" id="datatablesSimple" with="100%" collspacing="0">
+            <table class="table table-bordered" id="datatablesSimple" with="100%" collspacing="0"> 
                 <thead>
                     <tr>
                         <th>SN</th>
@@ -33,6 +32,22 @@
                         <th>Delete</th>
                     </tr>
                 </thead>
+                <tfoot>
+                    <tr>
+                    <th>SN</th>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Department</th>
+                        <th>Designation</th>
+                        <th>Start Date</th>
+                        <th>Address</th>
+                        <th>Mobile</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </tfoot>
                 <tbody>
                 @if(count($users)>0)
                     @foreach($users as $key => $user)
@@ -86,6 +101,19 @@
                 @endif
                 </tbody>
             </table>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-end">
+                    <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
 </div>
