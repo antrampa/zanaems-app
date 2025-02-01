3d70d6d2196eb98992d8b740c0304bcb06efc174
Antonis Zalonis <antonis.zalonis@gmail.com>
2025-02-01 16:38:39 +0200

    Added Permission into sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -47,6 +47,16 @@
                                             <a class="nav-link" href="{{route('users.index')}}">View Users</a>
                                         </nav>
                                     </div>
+                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapsePermissions" aria-expanded="false" aria-controls="pagesCollapsePermissions">
+                                        Permission 
+                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    </a>
+                                    <div class="collapse" id="pagesCollapsePermissions" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                            <a class="nav-link" href="{{route('permissions.create')}}">Create Permission</a>
+                                            <a class="nav-link" href="{{route('permissions.index')}}">View Permissions</a>
+                                        </nav>
+                                    </div>
                                 </nav>
                             </div>
                             <div class="sb-sidenav-menu-heading">Addons</div>
